e0b15a6495cc5f5d6e238730ea15bb1a056849f9
Dakota Clark <bannaray95@gmail.com>
2019-03-27 01:53:01 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,20 @@
-# Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text.downcase
+  end
+  
+  def count_of_words
+    words = text.split(" ")
+    woods.count
+  end
+  
+  def count_of_vowels
+    text.scan(/[aeoui]/).count
+  end
+  
+  def coun_of_consonants
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+  
